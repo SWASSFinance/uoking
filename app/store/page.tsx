aac2bb4b42754a084efce557fa5584c02542b323
SWASSFinance <sales@uoking.com>
2025-08-19 23:08:23 -0400

    sdfsfd

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -90,7 +90,7 @@ export default async function StorePage() {
           </div>
 
           {/* Categories Grid */}
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-6">
             {categoriesWithCounts.map((category: any) => {
               const IconComponent = categoryIcons[category.slug] || ShoppingBag;
               
